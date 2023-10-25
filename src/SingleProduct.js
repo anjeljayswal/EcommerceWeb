0347dbe4012e264ade00c10c9aedce456723803c
anjeljayswal <anjeljayswal@gmail.com>
2023-10-25 15:34:45 +0530

    rating in star

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -8,6 +8,7 @@ import { Container } from "./styles/Container";
 import FormatPrice from "./Helpers/FormatPrice";
 import { MdSecurity } from "react-icons/md";
 import { TbTruckDelivery, TbReplace } from "react-icons/tb";
+import Star from "./components/Star";
 // import MyImage from "./components/MyImage";
 
 const API = "https://api.pujakaitem.com/api/products";
@@ -52,8 +53,9 @@ const SingleProduct = () => {
           {/* product dAta  */}
           <div className="product-data">
             <h2>{name}</h2>
-            <p>{stars}</p>
-            <p>{reviews} reviews</p>
+            {/* <p>{stars}</p>
+            <p>{reviews} reviews</p> */}
+            <Star stars={stars} reviews={reviews} />
             <p className="product-data-price">
               MRP:
               <del>
