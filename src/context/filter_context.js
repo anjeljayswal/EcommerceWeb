54f2c15bd93aaaeaa5b3023a6d6b36568db60ede
anjeljayswal <anjeljayswal@gmail.com>
2023-11-04 12:48:10 +0530

    price sorting done

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -7,7 +7,7 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
   sorting_value: "lowest",
 };
 
@@ -26,13 +26,24 @@ export const FilterContextProvider = ({ children }) => {
     return dispatch({ type: "SET_LIST_VIEW" });
   };
 
+  //   sorting function 
+  const sorting = () => {
+    dispatch({ type: "GET_SORT_VALUE" });
+  };
+
+  // to sort the product 
+  useEffect(() => {
+    // console.log("hii");
+    dispatch({ type: "SORTING_PRODUCTS", payload: products });
+  }, [state.sorting_value])
+
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
     <FilterContext.Provider
-      value={{ ...state, setGridView,setListView }}>
+      value={{ ...state, setGridView, setListView, sorting }}>
       {children}
     </FilterContext.Provider>
   );
