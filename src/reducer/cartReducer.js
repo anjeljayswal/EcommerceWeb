4e3409435744a9368a8d6f8d4bfe4371ebb52ab6
anjeljayswal <anjeljayswal@gmail.com>
2023-12-02 16:48:01 +0530

    cart total price

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -105,56 +105,58 @@ const cartReducer = (state, action) => {
     };
   }
 
-  // if (action.type === "CART_TOTAL_ITEM") {
-  //   let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
-  //     let { amount } = curElem;
+  if (action.type === "CART_TOTAL_ITEM") {
+    let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+      let { amount } = curElem;
 
-  //     initialVal = initialVal + amount;
-  //     return initialVal;
-  //   }, 0);
+      initialVal = initialVal + amount;
+      return initialVal;
+    }, 0);
 
-  //   return {
-  //     ...state,
-  //     total_item: updatedItemVal,
-  //   };
-  // }
-
-  // if (action.type === "CART_TOTAL_PRICE") {
-  //   let total_price = state.cart.reduce((initialVal, curElem) => {
-  //     let { price, amount } = curElem;
-
-  //     initialVal = initialVal + price * amount;
-  //     // 25000 + 0 = 25000
-  //     // 10199 + 25000 = 121
-
-  //     return initialVal;
-  //   }, 0);
-
-  //   return {
-  //     ...state,
-  //     total_price,
-  //   };
-  // }
-
-  if(action.type === "CART_ITEM_PRICE_TOTAL"){
-    const {total_item, total_price} = state.cart.reduce(
-      (accum, curElem)=>{
-        const {price, amount} = curElem;
-        accum.total_item += amount;
-        accum.total_price += price * amount;
-
-        return accum;
-      },{
-      total_price:0,
-      total_item:0
-    });
-  
     return {
       ...state,
-      total_item,
-      total_price
+      total_item: updatedItemVal,
     };
   }
+
+  if (action.type === "CART_TOTAL_PRICE") {
+    let total_price = state.cart.reduce((initialVal, curElem) => {
+      let { price, amount } = curElem;
+
+      initialVal = initialVal + price * amount;
+      // 25000 + 0 = 25000
+      // 10199 + 25000 = 121
+
+      return initialVal;
+    }, 0);
+
+    return {
+      ...state,
+      total_price,
+    };
+  }
+
+  // if(action.type === "CART_ITEM_PRICE_TOTAL"){
+  //   const { total_item, total_price } = state.cart.reduce(
+  //     // (accum, curElem)=>{
+  //     //   console.log(accum, curElem);
+  //     //   const { price, amount } = curElem;
+  //     //   accum.total_item += amount;
+  //     //   accum.total_price += price * amount;
+
+  //     //   return accum;
+  //     // },
+  //     // {
+  //     // total_price:0,
+  //     // total_item:0
+  //   });
+  
+  //   return {
+  //     ...state,
+  //     total_item,
+  //     total_price
+  //   };
+  // }
 
   return state;
 };
